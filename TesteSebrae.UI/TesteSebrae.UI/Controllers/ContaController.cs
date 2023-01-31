aa6388256da23490f341da6f99671afa1f48de26
Ricardo Perdigão <rickyteck@hotmail.com>
2023-01-31 17:56:52 -0300

    [develop] - Inclusao do Razor

---
+++ TesteSebrae.UI/TesteSebrae.UI/Controllers/ContaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TesteSebrae.UI.Controllers
+{
+    public class ContaController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> List()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7194/");
+                var result = await client.GetAsync("api/Conta");
+
+                if (result.IsSuccessStatusCode)
+                {
+                    return View(result);
+                }
+            }
+            return View();
+        }
+    }
+}
